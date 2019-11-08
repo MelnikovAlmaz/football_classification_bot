4233984230fe9d14188aa53ebaa771dfdc3bffdf
Melnikov Almaz <melnikovalmaz@gmail.com>
2019-11-08 12:51:36 +0300

    Init basic telegram bot

---
+++ start.py
@@ -0,0 +1,18 @@
+import telebot
+
+import settings
+
+bot = telebot.TeleBot(settings.TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling()
